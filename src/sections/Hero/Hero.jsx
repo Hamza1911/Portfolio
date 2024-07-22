9c1bed8a317aa9fb29b4b280ee5099cc1c87c2c2
Hamza Javed <66196098+Hamza1911@users.noreply.github.com>
2024-07-22 12:34:25 +0500

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Design uten navn.png';
+import heroImg from '../../assets/Design uten navn (1).png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Hamza Javed"
         />
         <img
           className={styles.colorMode}
